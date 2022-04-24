5dba22e0c6efcf103f1f3b52b65b3fdb3d2d2a26
kazuki <pharcydetip@gmail.com>
2022-04-24 21:23:49 +0900

    add toInt constraint

--- src/main/scala/ndarray/shape.scala
+++ src/main/scala/ndarray/shape.scala
@@ -1,30 +1,28 @@
 package ndarray
 
-import shapeless.ops.hlist.Tupler
+import shapeless.ops.nat.ToInt
 import shapeless.{::, HList, HNil, Nat}
 
 trait Shape {
   type S <: HList
 
-  type Tuple = Tupler[S]#Out
-
   def shape: S
 }
 
 object Shape {
-  abstract class Shape1[N1 <: Nat: SummonNat] extends Shape {
+  class Shape1[N1 <: Nat: SummonNat: ToInt] extends Shape {
     type S = N1 :: HNil
 
     def shape: S = SummonNat[N1].value :: HNil
   }
 
-  abstract class Shape2[N1 <: Nat: SummonNat, N2 <: Nat: SummonNat] extends Shape {
+  class Shape2[N1 <: Nat: SummonNat: ToInt, N2 <: Nat: SummonNat: ToInt] extends Shape {
     type S = N1 :: N2 :: HNil
 
     def shape: S = SummonNat[N1].value :: SummonNat[N2].value :: HNil
   }
 
-  implicit def shape1[N1 <: Nat: SummonNat]: Shape1[N1]                           = new Shape1[N1] {}
-  implicit def shape2[N1 <: Nat: SummonNat, N2 <: Nat: SummonNat]: Shape2[N1, N2] =
-    new Shape2[N1, N2] {}
+  implicit def shape1[N1 <: Nat: SummonNat: ToInt]: Shape1[N1]                                  = new Shape1[N1]
+  implicit def shape2[N1 <: Nat: SummonNat: ToInt, N2 <: Nat: SummonNat: ToInt]: Shape2[N1, N2] =
+    new Shape2[N1, N2]
 }
