c7b154066a752aba6f92e75f2a017cf059f93884
kazuki <pharcydetip@gmail.com>
2022-04-24 14:02:29 +0900

    remoev non nothing

--- src/main/scala/ndarray/NdArray.scala
+++ src/main/scala/ndarray/NdArray.scala
@@ -3,18 +3,9 @@ package ndarray
 case class NdArray[T, Shape] private (value: Array[T]) {}
 
 object NdArray {
-  trait NonNothing[T]
-
-  object NonNothing extends NonNothing[Nothing] {
-    implicit def toNonNothing[T]: NonNothing[T] = new NonNothing[T] {}
-
-    implicit def ambiguous1: NonNothing[Nothing] = this
-    implicit def ambiguous2: NonNothing[Nothing] = this
-  }
 
   class ApplyPartiallyApplied[S <: Shape] {
-    def apply[T](value: Array[T])(implicit ev: NonNothing[S]): NdArray[T, S] =
-      new NdArray[T, S](value)
+    def apply[T](value: Array[T]): NdArray[T, S] = new NdArray[T, S](value)
   }
 
   def array[S <: Shape] = new ApplyPartiallyApplied[S]
